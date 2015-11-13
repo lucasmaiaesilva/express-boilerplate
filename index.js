1f93a16c94a1c79c110249603ab9e0985557a1de
Lucas Maia e Silva <lucasmaiaesilva@gmail.com>
2015-11-13 15:46:15 -0200

    adding bible feature

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    ptBible = require('bible-portuguese');
 
 app.get('/', function(req, res){
-    res.status(200).send("<h1>Hello World</h1>");
+    // res.status(200).send("<h1>Hello World</h1>");
+    ptBible.getVerse("Mateus 1:1", function(err, data){
+    	if (err) throw err;
+    	console.log(data);
+    });
 });
 
 app.get('*', function(req, res){
